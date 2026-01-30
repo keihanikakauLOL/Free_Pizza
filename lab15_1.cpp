2dbeb5d80a28a91b66a154dc9904b4e2678325f5
keihanikakauLOL <arm.nantana@gmail.com>
2026-01-30 15:10:51 +0700

    done

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,29 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int a = 5;
+	char b = 'A';
+	char &c = b;
 
+	int *px = &a;
+	char *py = &b;
+	int **pz = &px;
+
+	cout << a << " " << b << " " << c << " " << px << " " << (int *) &py << " " << pz << endl;
+	cout << &a << " " << (int *) &b << " " << (int *) &c << " " << &px << " " << &py << " " << &pz << endl;
 	
+	c = 'F';
+	cout << a << " " << b << " " << c << " " << px << " " << (int *) &py << " " << pz << endl;
+
+	*py = 'W';
+	cout << a << " " << b << " " << c << " " << px << " " << (int *) &py << " " << pz << endl;
+
+	*px = 6;
+	cout << a << " " << b << " " << c << " " << px << " " << (int *) &py << " " << pz << endl;
+
+	**pz = 7;
+	cout << a << " " << b << " " << c << " " << px << " " << (int *) &py << " " << pz << endl;
 	
 	return 0;
 }
